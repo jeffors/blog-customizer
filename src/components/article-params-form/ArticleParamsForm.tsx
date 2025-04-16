502843de56a84d6bc9e8e1c899e5f32e9400c2d9
Azamat Safin <register.uzik@yandex.ru>
2025-04-16 19:54:43 +0500

    Начинаем разбираться с React: для начала откроем и закроем панель

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,11 +2,23 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	function handleClick() {
+		setIsOpen(!isOpen);
+	}
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
+			<ArrowButton
+				isOpen={isOpen}
+				onClick={() => {
+					handleClick();
+				}}
+			/>
 			<aside className={styles.container}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
